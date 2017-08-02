35a162ff6b72a4b70ac91d0585d6f0ffc08738ce
Manish-Rythmos <mbhakuni@rythmos.com>
2017-08-02 15:13:33 +0530

    commit to master, added .net xunit project

---
+++ WebAPITests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.Controllers;
+using Xunit;
+
+namespace WebAPITests.Controllers
+{     
+    public class ValuesControllerTest
+    {
+        [Fact]
+        public void Get()
+        {
+            //// Arrange
+            //ValuesController controller = new ValuesController();
+
+            //// Act
+            //IEnumerable<string> result = controller.Get();
+
+            //// Assert
+            //Assert.IsNotNull(result);
+            //Assert.AreEqual(2, result.Count());
+            //Assert.AreEqual("value1", result.ElementAt(0));
+            //Assert.AreEqual("value2", result.ElementAt(1));
+
+            // Assert
+            Assert.Equal(5, 5);
+        }
+    }
+}
