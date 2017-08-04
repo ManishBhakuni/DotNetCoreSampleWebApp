d46466521202cb4435228661e58a3b1d8d667b52
ManishBhakuni <patexample11@gmail.com>
2017-08-04 12:14:17 +0530

    Update CoverageReport.ps1

--- CoverageReport.ps1
+++ CoverageReport.ps1
@@ -5,4 +5,4 @@ nuget restore -PackagesDirectory .\packages;
 dotnet publish .\WebAPITests -o ..\build_tests --framework netcoreapp1.1;
 ls;
 .\packages\opencover\4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"vstest WebAPITests.dll" -targetdir:"build_tests" -oldStyle -register:user -output:"_CodeCoverageResult.xml" -filter:"+[WebAPI]*";
-.\packages\ReportGenerator\2.5.9\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport";
+.\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport";
