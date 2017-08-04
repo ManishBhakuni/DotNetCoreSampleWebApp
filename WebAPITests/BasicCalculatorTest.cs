a2290712961d584ea68de3070f84d9793172fd16
Manish-Rythmos <mbhakuni@rythmos.com>
2017-08-04 16:41:20 +0530

    added new test function

--- WebAPITests/BasicCalculatorTest.cs
+++ WebAPITests/BasicCalculatorTest.cs
@@ -13,9 +13,14 @@ namespace WebAPITests
         [Fact]
         public void SumTest()
         {
-            Assert.Equal(9, objBasicCalculator.Sum(5, 4));           
+            Assert.Equal(12, objBasicCalculator.Sum(5, 7));           
         }
 
+        [Fact]
+        public void SubtractionTest()
+        {
+            Assert.Equal(10, objBasicCalculator.Subtraction(16, 6));
+        }
 
     }
 }
