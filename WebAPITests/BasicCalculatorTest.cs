b8c7f519fe4d14cdcf023c24d29e7ba90b24ce2f
Manish-Rythmos <mbhakuni@rythmos.com>
2017-08-07 21:18:59 +0530

    add same test case again

--- WebAPITests/BasicCalculatorTest.cs
+++ WebAPITests/BasicCalculatorTest.cs
@@ -15,6 +15,12 @@ namespace WebAPITests
         {
             Assert.Equal(12, objBasicCalculator.Sum(2, 10));           
         }
-       
+
+        [Fact]
+        public void SubtractionTest()
+        {
+            Assert.Equal(10, objBasicCalculator.Subtraction(16, 6));
+        }
+
     }
 }
