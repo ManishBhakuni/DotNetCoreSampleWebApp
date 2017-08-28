b83e2947a5353be138d4a1e5f46fc264fb266327
ManishBhakuni <patexample11@gmail.com>
2017-08-28 18:34:23 +0530

    add new files for custom appsettings

---
+++ WebAPI/CustomAppSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class CustomAppSetting
+    {
+        public string TokenEndPoint { get; set; }
+    }
+}
