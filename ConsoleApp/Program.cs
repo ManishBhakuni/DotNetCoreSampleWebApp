f300d62d72b3f9b5122824d3b61ea1613bca6d85
ManishBhakuni <patexample11@gmail.com>
2017-08-29 21:49:50 +0530

    add new console app project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var settings = new CustomAppSetting();
+            configuration.Bind(settings);
+
+            var value1 = settings.ApplicationName;
+            Console.WriteLine(value1);
+            Console.WriteLine($"My application name is '{settings.ApplicationName}'");
+
+            Console.ReadLine();
+
+        }
+    }
+}
