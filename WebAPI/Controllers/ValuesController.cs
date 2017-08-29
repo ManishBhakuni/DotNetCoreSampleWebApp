b0cc80bdd0d9f0fd9dc98db2ddfd715210e428fb
ManishBhakuni <patexample11@gmail.com>
2017-08-29 14:34:06 +0530

    update appsetting.json file

--- WebAPI/Controllers/ValuesController.cs
+++ WebAPI/Controllers/ValuesController.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        //IConfiguration _iConfiguration;
+        //public ValuesController(IConfiguration iConfiguration) {
+        //    _iConfiguration = iConfiguration;
+        //}
+
+        private readonly CustomAppSetting config;
+
+        public ValuesController(IOptions<CustomAppSetting> iConfiguration)
+        {
+            config = iConfiguration.Value;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };                      
+            string value1 = config.TokenEndPoint;   
+            return new string[] { value1, "value2" };                      
         }
 
         // GET api/values/5
@@ -23,6 +38,10 @@ namespace WebAPI.Controllers
             return "value";
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
